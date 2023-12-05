f3dab9911da7a2f57e6dad9462343ea8e49da1bc
DatDong <phatdat16102002@gmail.com>
2023-12-05 20:13:15 +0700

    Buoi 1: Tong quan Js

---
+++ TongQuanJS/ThucHanh/BTTL-D1-01.js
@@ -0,0 +1,15 @@
+function showMessageAlert() {
+    alert('Vui qua biet code roi');
+}
+
+function showMessage() {
+    document.getElementById("messageDiv").innerHTML = 'show message innerhtml';
+}
+
+function showMessageWrite() {
+    document.write("Lai code them duoc dong moi roi");
+}
+
+function showMessageConsole() {
+    console.log("Lai code them duoc dong moi roi");
+}
