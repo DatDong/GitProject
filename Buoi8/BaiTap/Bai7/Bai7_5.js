bef3042575c70537891856e5a3d00749521c184f
DatDong <phatdat16102002@gmail.com>
2024-01-09 20:48:35 +0700

    done buoi9

--- Buoi8/BaiTap/Bai7/Bai7_5.js
+++ Buoi8/BaiTap/Bai7/Bai7_5.js
@@ -1,12 +1,17 @@
 const a = [1, "asd", 2, 3, "sas", 4, 5, 6];
 const b = [1, "asd", 2, 3, "sas", 4, 5, 6];
 
+let isEquale = true;
 for (let i = 0; i < a.length; i++) {
   if (a[i] !== b[i]) {
     console.log("2 mang nay khac nhau");
-    break;
-  } else {
-    console.log("2 mang nay giong nhau");
+    isEquale = false;
     break;
   }
+}
+
+if (isEquale) {
+  console.log("2 mang nay giong nhau");s
+} else {
+  console.log("2 mang nay khac nhau");
 }
